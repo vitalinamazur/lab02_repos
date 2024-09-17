a383ae1f4f29bed297c3b9c9e128ce8e0dcd9501
Vitalina <vitalinamazur296@gmail.com>
2024-09-17 20:38:52 +0300

    first master commit

---
+++ LabAP_2/LabAP_2.cpp
@@ -0,0 +1,28 @@
+// Lab_02.cpp
+// <Мазур Віталіни>
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 19
+
+#include <iostream>
+#include <cmath> // підключення бібліотеки для пропису степенів формулою 'paw'
+
+using namespace std;
+
+int main()
+{
+    double a;
+    double z1;
+    double z2;
+
+    cout << "a = "; cin >> a;
+
+    z1 = pow(((1 + a + pow(a, 2)) / (2 * a + pow(a, 2)) + 2 - (1 - a + pow(a, 2)) / (2 * a - pow(a, 2))), -1) * (5 - 2 * pow(a, 2));
+    z2 = (4 - pow(a, 2)) / 2;
+
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    return 0;
+}
+//Перша зміна
